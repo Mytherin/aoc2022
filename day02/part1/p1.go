c320684f0d7bdfc98c4bf0630615d7d623415041
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-12-02 08:31:53 +0100

    day2

---
+++ day02/part1/p1.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+// X for Rock, Y for Paper, and Z for Scissors
+// The winner of the whole tournament is the player with the highest score.
+// Your total score is the sum of your scores for each round.
+// The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+// plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
+type Move int
+
+const (
+    Rock Move    = 1
+    Paper        = 2
+    Scissors     = 3
+)
+
+func compute_score(us Move, them Move) int {
+    switch us {
+    case Rock: {
+        switch them {
+        case Rock:
+            return 3 + 1 // draw
+        case Paper:
+            return 0 + 1 // loss
+        case Scissors:
+            return 6 + 1 // win
+        }
+    }
+    case Paper: {
+        switch them {
+        case Rock:
+            return 6 + 2 // win
+        case Paper:
+            return 3 + 2 // draw
+        case Scissors:
+            return 0 + 2 // loss
+        }
+    }
+    case Scissors: {
+        switch them {
+        case Rock:
+            return 0 + 3 // loss
+        case Paper:
+            return 6 + 3 // win
+        case Scissors:
+            return 3 + 3 // draw
+        }
+    }
+    }
+    panic("Unrecognized input")
+}
+
+func parse_move(s byte) Move {
+    if s == byte('A') || s == byte('X') {
+        return Rock
+    } else if s == byte('B') || s == byte('Y') {
+        return Paper        
+    } else if s == byte('C') || s == byte('Z') {
+        return Scissors
+    }
+
+    panic("Unrecognized move")
+}
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    score := 0
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        them := parse_move(line[0])
+        us := parse_move(line[2])
+        score += compute_score(us, them)
+    }
+    fmt.Println("Score: %d\n", score)
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
