9c673689a29ddf543845fc89b5e6ce4306708562
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-12-12 22:17:55 +0100

    day12

---
+++ day12/part1/p1.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+type GridNode struct {
+    height int
+    score int
+}
+
+type Node struct {
+    x int
+    y int
+}
+
+func get_grid_node(grid [][]GridNode, x int, y int) GridNode {
+    return grid[y][x]
+}
+
+func should_add_node(c GridNode, n GridNode) bool {
+    return n.score > c.score + 1 && n.height <= c.height + 1
+}
+
+func get_neighbors(grid [][]GridNode, x int, y int) []Node {
+    result := make([]Node, 0, 10)
+    c := get_grid_node(grid, x, y)
+
+    if x > 0 {
+        n := get_grid_node(grid, x - 1, y)
+        if should_add_node(c, n) {
+            result = append(result, Node { x: x - 1, y : y })
+        }
+    }
+    if x + 1 < len(grid[0]) {
+        n := get_grid_node(grid, x + 1, y)
+        if should_add_node(c, n) {
+            result = append(result, Node { x: x + 1, y : y })
+        }
+    }
+    if y > 0 {
+        n := get_grid_node(grid, x, y - 1)
+        if should_add_node(c, n) {
+            result = append(result, Node { x: x, y : y - 1 })
+        }
+    }
+    if y + 1 < len(grid) {
+        n := get_grid_node(grid, x, y + 1)
+        if should_add_node(c, n) {
+            result = append(result, Node { x: x, y : y + 1 })
+        }
+    }
+    return result
+}
+
+func find_path(grid [][]GridNode, nodes []Node, end_x int, end_y int) int {
+    for i := 0; i < len(nodes); i++ {
+        x := nodes[i].x
+        y := nodes[i].y
+        score := get_grid_node(grid, x, y).score
+
+        neighbors := get_neighbors(grid, x, y)
+        for n_idx := 0; n_idx < len(neighbors); n_idx++ {
+            n := neighbors[n_idx]
+
+            grid[n.y][n.x].score = score + 1
+            nodes = append(nodes, n)
+        }
+    }
+    if grid[end_y][end_x].score == 99999 {
+        panic("No path found?")        
+    }
+    return grid[end_y][end_x].score
+}
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    grid := make([][]GridNode, 0, 10)
+    var start Node
+    var end_x int
+    var end_y int
+    for scanner.Scan() {
+        line := scanner.Text()
+        result := make([]GridNode, 0, 10)
+        for x := 0; x < len(line); x++ {
+            var character byte
+            if line[x] == 'S' {
+                start.x = x
+                start.y = len(grid)
+                character = 'a'
+            } else if line[x] == 'E' {
+                end_x = x
+                end_y = len(grid)
+                character = 'z'
+            } else {
+                character = line[x]
+            }
+            if character >= 'a' && character <= 'z' {
+                var new_node GridNode
+                new_node.height = int(character - 'a')
+                new_node.score = 99999
+                result = append(result, new_node)
+            } else {
+                panic("unrecognized letter")
+            }
+        }
+        grid = append(grid, result)
+    }
+    grid[start.y][start.x].score = 0
+    nodes := make([]Node, 0, 10)
+    nodes = append(nodes, start)
+    shortest_path := find_path(grid, nodes, end_x, end_y)
+    fmt.Println(shortest_path)
+
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
